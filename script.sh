8341d588b558fe8cddfbeb4920bc3d49bfe7e01c
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-09-08 12:20:31 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
-
-echo 'touch ~/fresh.txt
+str='touch ~/fresh.txt
 freshfetch < ~/fresh.txt
 cat ~/fresh.txt
-rm ~/fresh.txt' >> ~/.bashrc
+rm ~/fresh.txt'
+echo $str >> ~/.bashrc
 echo 'This appends the alias to ~/.bashrc. Restart bash(just restart your terminal/pc) if it doesn't work'
 echo 'errors occur for some reason despite the echo command working 100%'
