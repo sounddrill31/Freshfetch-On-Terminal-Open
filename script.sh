141ec5f9b43607c34752b129f7dea808690325ba
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-09-08 12:10:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,9 @@ freshfetch < ~/fresh.txt
 cat ~/fresh.txt
 rm ~/fresh.txt
 
-echo 'alias dog=nano' >> ~/.bashrc
+echo 'touch ~/fresh.txt
+freshfetch < ~/fresh.txt
+cat ~/fresh.txt
+rm ~/fresh.txt
+' >> ~/.bashrc
 echo 'This appends the alias to ~/.bashrc. Restart bash(just restart your terminal/pc) if it doesn't work'
