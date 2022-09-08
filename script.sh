1f8ad175ea30f200fc3d9e8d56df17020692d8bb
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-09-08 13:08:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 echo echo $(freshfetch) >> ~/.bashrc
 echo 'This prepends(adds to top of file) the alias to ~/.bashrc. Restart bash(just restart your terminal/pc) if it doesn't work'
 echo 'errors occur for some reason despite the echo command working 100%'
-sed -i '1s/^/touch ~/fresh.txt && freshfetch < ~/fresh.txt && cat ~/fresh.txt && rm ~/fresh.txt\n/' ~/.bashrc
+echo '1s/^/touch ~/fresh.txt && freshfetch < ~/fresh.txt && cat ~/fresh.txt && rm ~/fresh.txt\n/' ~/.bashrc
 
 
